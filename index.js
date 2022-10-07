6efd1484ac77cd6f1cf2798361f115eb291cdd19
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-07 16:51:47 -0400

    RE: prep for deployment.Cleaned up env variables

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.get('/', (req, res) => {
     res.send('APP IS RUNNING')
 });
 
-const CONNECTION_URL = 'mongodb+srv://dontrelldev:KNight1990$@cluster0.gpbso8p.mongodb.net/?retryWrites=true&w=majority'
+
 
 const PORT = process.env.PORT|| 3010;
 
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
