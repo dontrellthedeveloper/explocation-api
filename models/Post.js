30fa87609379c3cb6439c1d9d4c71d94ab382250
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-07 16:20:39 -0400

    prepared server for production

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: {
+        type: String,
+        // maxLength: 100
+    },
+    likes: {
+        type: [String],
+        default: []
+    },
+    comments: {
+        type: [String],
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
