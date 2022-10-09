098bf2ace00b63497ae89533db669847ef719e64
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-09 02:23:04 -0400

    added cloudinary for file upload

--- models/Post.js
+++ models/Post.js
@@ -7,7 +7,15 @@ const postSchema = mongoose.Schema({
     creator: String,
     tags: [String],
     selectedFile: {
-        type: String,
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+        // type: String,
         // maxLength: 100
     },
     likes: {
